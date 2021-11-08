cfbeee1fef0f33e928ea9b544d852ffbfa636539
hugoresende27 <Hugoresende27@gmail.com>
2021-11-08 21:33:46 +0000

    a

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
 const piada = document.getElementById('joke')
-const piada = document.getElementById('jokeBtn')
+const piadaBtn = document.getElementById('jokeBtn')
 
-gerarPiada()
+piadaBtn.addEventListener('click', gerarPiada)
 
-function gerarPiada() {
+// gerarPiada()
+
+// function gerarPiada() {
+//     const config = {
+//         headers : {
+//             'Accept' : 'application/json'
+//         }
+//     }
+//     fetch('https://icanhazdadjoke.com',config)//usou-se a var config para guardar o objeto criado com o fetch
+//     .then((res) => res.json())
+//     .then((data) => {//console.log(data))//print na consola do objeto piada
+//         piada.innerHTML = data.joke //piada vai receber por innerHTML apenas joke do objeto piada
+//     })
+// }
+
+/********EM BAIXO O MESMO DO QUE EM CIMA, COM UMA ESCRITA MAIS LIMPA USANDO async E await  */ 
+
+async function gerarPiada() {
     const config = {
         headers : {
             'Accept' : 'application/json'
         }
     }
-    fetch('https://icanhazdadjoke.com',config)
+  const res = await fetch('https://icanhazdadjoke.com',config)
+
+  const data = await res.json()
+  piada.innerHTML = data.joke
 }
